f636525b80ff7cbd005d6318917c585f1d5de708
Onyedikachi Eze <thekacchi1@gmail.com>
2023-08-11 15:43:29 -0700

    0kay

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-print(f"{a} + {b} = {add(a, b)}")
+
+if __name__ == "__main__':
+    """Print sum of 1 & 2"""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
