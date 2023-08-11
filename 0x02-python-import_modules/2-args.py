406bf6773dfd5b0208bc0c401f30ba1953d61654
Onyedikachi Eze <thekacchi1@gmail.com>
2023-08-11 16:57:05 -0700

    t2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+def main():
+    count = len(sys.argv) - 1
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(count))
+        s_num = 1
+        for arg in range (count):
+            print("{}: {}".format(s_num, sys.argv[arg + 1]))
+            s_num = s_num + 1
+
+if __name__ == "__main__":
+    main()
