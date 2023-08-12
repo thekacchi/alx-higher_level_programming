77ac5cd28aee2c8d9fdb59a07057367f04d095dd
Onyedikachi Eze <thekacchi1@gmail.com>
2023-08-11 17:38:53 -0700

    t3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def main():
+    count = len(sys.argv) - 1
+    if count == 0:
+        print(0)
+    elif count > 0:
+        summ = 0
+        i = 1
+        for i in range(count):
+            asum = sys.argv[i + 1]
+            summ = summ + int(asum)
+            i = i + 1
+
+        print(summ)
+
+if __name__ == "__main__":
+    main()
