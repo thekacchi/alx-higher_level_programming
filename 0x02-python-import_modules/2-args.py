dceec4484b327ca7d203fbc944ceb1a9a5c9ba7f
Onyedikachi Eze <thekacchi1@gmail.com>
2023-08-11 17:02:12 -0700

    wake me

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def main():
     count = len(sys.argv) - 1
 
@@ -13,9 +14,10 @@ def main():
     else:
         print("{} arguments:".format(count))
         s_num = 1
-        for arg in range (count):
+        for arg in range(count):
             print("{}: {}".format(s_num, sys.argv[arg + 1]))
             s_num = s_num + 1
+
 
 if __name__ == "__main__":
     main()
