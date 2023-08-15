cf4dad7e85808f9e9b1d26064a44abddc9733723
Onyedikachi Eze <thekacchi1@gmail.com>
2023-08-14 20:46:54 -0700

    t1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bib/python3
+
+def element_at(my_list, idx):
+    """Retrieve an element of a lost"""
+    if idx < 0 or idx > (len(my_list) - 1):
+	    return None
+    return (my_list[idx])
