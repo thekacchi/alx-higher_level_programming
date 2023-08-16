9ae9233974285b2555960fbe3d08b8885d0536a8
Onyedikachi Eze <thekacchi1@gmail.com>
2023-08-15 20:28:10 -0700

    e4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 
 def new_in_list(my_list, idx, element):
     """replace an element in a copied list at specific poaitiob"""
-    if idx < 0 or idx < (len(my_list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return(my_list)
 
     copy = [x for x in my_list]
