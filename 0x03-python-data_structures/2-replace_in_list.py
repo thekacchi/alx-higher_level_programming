734b043fb5a1ad95e6302f670429e55dd5f7bdfe
Onyedikachi Eze <thekacchi1@gmail.com>
2023-08-15 20:21:05 -0700

    crime

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@
 
 def replace_in_list(my_list, idx, element):
     """Replace item in list"""
-    if idx >= 0 or idx < (len(my_list) - 1):
+    if idx >= 0 and idx < (len(my_list) - 1):
         my_list[idx] = element
     return (my_list)
