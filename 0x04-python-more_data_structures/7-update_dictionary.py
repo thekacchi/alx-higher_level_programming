73e513b145fcef9670a6d440fc8e24c71ee451fd
Onyedikachi Eze <thekacchi1@gmail.com>
2023-08-16 18:55:08 -0700

    8

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if isinstance(key, str) and key != "":
-        if a_dictionary is not None:
-            a_dictionary[key] = value
-
+    a_dictionary[key] = value
+    return (a_dictionary)
