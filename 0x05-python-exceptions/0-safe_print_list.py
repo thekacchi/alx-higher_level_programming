cfd19528798eb821398a394eadb9402e3e8a902a
Onyedikachi Eze <thekacchi1@gmail.com>
2023-08-29 17:20:11 -0700

    t0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for item in my_list:
+            if count < x:
+                print(item, end=" ")
+                count += 1
+        print()
+        return count
+    except:
+        return count
