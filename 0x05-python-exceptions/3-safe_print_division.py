0ed0558450a1ddd76a67f8d7cf84228ed317c5ea
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-03 15:29:26 -0700

    h3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,9 @@ def safe_print_division(a, b):
     try:
         result = a/b
         print("Inside result: {:.1f}".format(result))
-        return result
     except ZeroDivisionError:
-        print("Inside resilt: None")
-        return None
+        result = "None"
+        print("Inside result: None")
+        return result
+    finally:
+        return result
