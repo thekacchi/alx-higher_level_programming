03ec10be8203e3b74c3f9dc0051012304e595ded
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-03 10:52:46 -0700

    safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         for item in my_list:
             if count < x:
-                print(item, end=" ")
+                print(item, end="")
                 count += 1
             else:
                 break
