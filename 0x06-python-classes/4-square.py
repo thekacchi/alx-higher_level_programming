3b63c5e6c5dafe392f0350951cc541e608bbefcc
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-04 16:46:41 -0700

    task 4 complete

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,12 +14,16 @@ class Square:
     def area(self):
         return self._size * self._size
 
+    @property
     def size(self):
+        """Getter method for size attribute"""
         return self._size
 
+    @size.setter
     def size(self, value):
+        """Setter method for size attribute"""
         if isinstance(value, int) is False:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self._size = value
