d9b79a77e24e1495bb73de93437b3eb5ebb5f640
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-05 17:55:53 -0700

    t19

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    This module defines and declares func
+"""
+""" copy_list copies thelist"""
+
+
+def copy_list(l=[]):
+    new_l = [item for item in l]
+
+    return new_l
