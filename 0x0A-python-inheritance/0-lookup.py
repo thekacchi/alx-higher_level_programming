328a5408954c522343fa0165ed25304c7a7505f8
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-11 16:54:06 -0700

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    This module defines lookup
+    """
+
+
+def lookup(obj):
+    """
+    This function lists all attributes and methods of a class
+    """
+    return dir(obj)
