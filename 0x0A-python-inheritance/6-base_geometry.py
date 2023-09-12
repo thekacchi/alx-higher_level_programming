e7f9b9d9643f63bb0811a5c09281a554c0dae1a1
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-11 18:23:54 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    This module defines BG Class
+    """
+
+
+class BaseGeometry():
+    """
+    Basegeometry class
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
