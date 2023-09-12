83dfa5facdd0589e9046043df6ba1df848c6b19f
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-12 12:08:58 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module contains a funtion that reads a file"""
 
+
 def read_file(filename=""):
     """This function reads a file's content"""
-    with open(filename, 'r', encoding= 'utf-8') as file:
+    with open(filename, 'r', encoding = 'utf-8') as file:
         for line in file:
             print(line, end='')
