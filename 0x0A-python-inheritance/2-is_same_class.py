25b67089cb74899a5ae85d7c08cf3d5ce185c388
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-11 17:25:07 -0700

    T2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+   This module defines a function
+"""
+
+def is_same_class(obj, a_class):
+    """
+    This function checks if an object obj is an instance of specified a_class
+    """
+    return type(obj) is a_class
