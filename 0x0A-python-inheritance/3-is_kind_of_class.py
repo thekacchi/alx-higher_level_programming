73c69c9ab626ce86365db28fbc955bbf6485c8bd
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-11 17:32:05 -0700

    task3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+   This module defines a function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    This function checks if an object obj is an instance of specified a_class
+    """
+    return isinstance(obj, a_class)
