2beeae9e7a3522f093964362baf5de1fa4d014a1
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-12 12:21:37 -0700

    task 1 done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """This function reads a file's content"""
-    with open(filename, 'r', encoding = 'utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains the definition for write function"""
+
+
+def write_file(filename="", text=""):
+    """Function defiens a function that writes text in filename"""
+    try:
+        with open(filename, 'w', encoding = 'utf8') as file:
+            num_characters = file.write(text)
+        return num_characters
+    except Exception:
+        return 0
