3a7d56ca1c7dbbab08ab068d992220196b0ba0b7
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-12 17:42:25 -0700

    task 6 ok

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bon/python3
+"""This module contains function definition"""
+import json
+
+
+def load_from_json_file(filename):
+    """This function creates object from JSON file"""
+    with open(filename, 'r', encoding='utf8') as file:
+        data = json.load(file)
+    return data
