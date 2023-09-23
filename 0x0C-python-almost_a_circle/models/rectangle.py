da456a6765430ccd28755df2ed22b4f1070c6133
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-23 03:02:22 -0700

    to dict for rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,3 +114,13 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return a doxtionary representation of the square"""
+        return {
+            "id": self.id,
+            "width": self.__width,
+            "height": self.__height,
+            "x": self.__x,
+            "y": self.__y
+        }
