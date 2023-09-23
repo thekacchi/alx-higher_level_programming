f84a622d5e0c01569a125dcf35feab8d7a563ba3
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-23 05:31:34 -0700

    json dumps and load

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines the base"""
+import json
 
 
 class Base:
@@ -13,3 +14,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Serializes a list of dictionaries into JSON string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    def from_json_string(json_string):
+        """Returns list of dictionary from json string
+        Args: json string rep of list dictionaries
+        """
+        if not json_string:
+            return []
+        return json.loads(json_string)
