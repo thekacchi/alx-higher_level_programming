7b7cfa0f1e1cf4e3cb8d67d23e0b6304cd83a251
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-23 02:32:00 -0700

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module is for square, which inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Definition of the class Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes the square instance"""
+        super().__init__(size, size, x, y, id)
+        """Call the rectangle instructor with size as idth and height"""
+
+    def __str__(self):
+        """Returns string representationmof square"""
+        return "[Square] ({}) {}/{} - {}".format(
+               self.id, self.x, self.y, self.width)
