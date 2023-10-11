6a8ce0018b42a3e7e86f036a79eca37952fc4d3e
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-11 11:43:31 -0700

    say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python 3
+"""The say my name module creates a function"""
+
+def say_my_name(first_name, last_name=""):
+    """This function prints first_name and last_name
+
+    Args:
+        first_name: first name string
+        last_name: last name string
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+    print('{} {}'.format(first_name, last_name))
