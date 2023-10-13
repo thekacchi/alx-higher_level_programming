51aace8644d348b45666f712895ff79aa9956e5e
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-12 23:10:30 -0700

    fixed code

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,9 +11,18 @@ def text_indentation(text: str):
     if type(text) is not str:
         raise TypeError('text must be a string')
 
+    special_chars = ['.', '?', ':']
+    result = []
+
     for c in text:
-        if c == '.' or c == '?' or c == '!' or c == ":":
-            print(c, end="")
-            print('\n\n', end="")
-            continue
-        print(c, end="")
+        result.append(c)
+        if c in special_chars:
+            result.append('\n\n')
+
+    print("".join(result))
+#    for c in text:
+#        if c == '.' or c == '?' or c == '!' or c == ":":
+#            print(c, end="")
+#            print('\n\n', end="")
+#            continue
+#        print(c, end="")
