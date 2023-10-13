de1b6aeb026458c4bbd95cdf5077cc4516cb7b61
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-12 22:37:48 -0700

    text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module is documented"""
+
+
+def text_indentation(text: str):
+    """Prints a text with two newlines after each characters: ., ? and :
+
+    Arguments:
+    text: The string to be printed
+    """
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    for c in text:
+        if c == '.' or c == '?' or c == '!':
+            print('\n\n', end="")
+            continue
+        print(c, end="")
