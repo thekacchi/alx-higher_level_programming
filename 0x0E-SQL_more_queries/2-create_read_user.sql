4cc44953c38140ccdf0ff5b1886cd10c40c2c6e3
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-18 16:41:21 -0700

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,11 @@
 -- Create 'user_0d_1' if it doesn't exist
 CREATE USER IF NOT EXISTS
-'user_0d_1'@'localhost' IDENTIFIED BY
-'user_0d_1_pwd';
+'user_0d_2'@'localhost' IDENTIFIED BY
+'user_0d_2_pwd';
 
 -- Grant all privileges to 'user_0d_1'
 GRANT ALL PRIVILEGES ON *.* TO
-'user_0d_1'@'localhost' WITH GRANT OPTION;
+'user_0d_2'@'localhost' WITH GRANT OPTION;
 
 -- Flush provileges to apply changes
 FLUSH PRIVILEGES
