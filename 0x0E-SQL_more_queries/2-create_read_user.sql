7f8e928fc69eb9688c84a045cb37474c56592d63
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-18 16:48:21 -0700

    you

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,15 @@
+-- Create the database if it doesn't exist
+CREATE DATABAE IF NOT EXISTS hbtn_0d_2;
+
 -- Create 'user_0d_1' if it doesn't exist
 CREATE USER IF NOT EXISTS
 'user_0d_2'@'localhost' IDENTIFIED BY
 'user_0d_2_pwd';
 
--- Grant all privileges to 'user_0d_1'
-GRANT ALL PRIVILEGES ON *.* TO
-'user_0d_2'@'localhost' WITH GRANT OPTION;
+-- Grant USAGE privileg to 'user_0d_1' on all databases
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+
+GRANT SELECT ON 'hbtn_0d_2.' TO 'user_0d_2'@'localhost';
 
 -- Flush provileges to apply changes
 FLUSH PRIVILEGES
