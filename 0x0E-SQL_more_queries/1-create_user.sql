7565b09163feb52709dfff6d0a6b52a6cdef5fc2
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-18 16:22:34 -0700

    1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,11 @@
+-- Create 'user_0d_1' if it doesn't exist
+CREATE USER IF NOT EXISTS
+'user_0d_1'@'localhost' IDENTIFIED BY
+'user_0d_1_pwd';
+
+-- Grant all privileges to 'user_0d_1'
+GRANT ALL PRIVILEGES ON *.* TO
+'user_0d_1'@'localhost';
+
+-- Flush provileges to apply changes
+FLUSH PRIVILEGES
