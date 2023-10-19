b6bec9fac8f423ef2d15655431688024c4726987
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-19 14:46:59 -0700

    temp

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Displays the average temperature in Farenheit, in descending order
-SELECT city, AVG(value) AS acg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY acg_temp DESC;
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
