bacc27bd746cb0f135c1be5e0d1b6bb7c59cd905
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-19 05:55:50 -0700

    hey

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- Prints the full description of the first_table from the database
+CREATE TABLE IF NOT EXISTS first_table (
+	id int DEFAULT NULL,
+	name varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci
