d14746ddf390bd1f970b463aaf0eb6297393e0a0
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-20 03:12:59 -0700

    11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- List all shows in the database, display NULL for shows without genres.
+SELECT s.title, g.genre_id
+FROM tv_shows AS s
+INNER JOIN tv_show_genres AS g
+ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
