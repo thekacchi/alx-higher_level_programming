2e7d09423f1b36f08258d47d30b4aecafb8cd96e
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-20 02:58:42 -0700

    cali

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,10 @@
 -- Use the hbtn.. database
 USE hbtn_0d_usa;
 
-SELECT cities.id, cities.state_id
+SELECT id, name
 FROM cities
-WHERE cities.state_id = (SELECT id FTOM states WHERE name = 'California')
-ORDER BY cities.id;
+WHERE state_id IN
+	(SELECT id
+		FROM states
+		WHERE name = 'California')
+ORDER BY id;
