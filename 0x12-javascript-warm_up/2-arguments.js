edd9d1ce250991f2e003cceb631b4d88e55a09c5
Onyedikachi Eze <thekacchi1@gmail.com>
2023-11-13 15:00:58 -0800

    print arg

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,15 +2,10 @@
 
 const argcount = process.argv.length - 2;
 
-if (argcount === 0)
-{
-	console.log('No argument');
-}
-else if (argcount === 1)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('Arguments found');
+if (argcount === 0) {
+  console.log('No argument');
+} else if (argcount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
