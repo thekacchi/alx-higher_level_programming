dea10b5178122f27963637ff59208dde82a098a5
Onyedikachi Eze <thekacchi1@gmail.com>
2023-11-14 14:45:51 -0800

    3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Rectangle = class {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return Object.create(Rectangle.prototype);
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
+
+module.exports = Rectangle;
