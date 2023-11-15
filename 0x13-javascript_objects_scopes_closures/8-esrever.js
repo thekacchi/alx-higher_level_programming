eb086cb4968811b8609df42bec4f6711603a2bef
Onyedikachi Eze <thekacchi1@gmail.com>
2023-11-15 14:15:43 -0800

    task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const reversed = []
+  let i = list.length - 1;
+
+  while (i >= 0) {
+    reversed.push(list[i]);
+    i--;
+  };
+  return reversed;
+};
