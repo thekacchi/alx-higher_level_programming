c6fbe5601231760574df5973761590edbfa50ed6
Onyedikachi Eze <thekacchi1@gmail.com>
2023-12-05 13:43:07 -0800

    lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-url=$1
-url -sI "$url" | grep -i Content-Length | awk '{print $2}' | tr -d '\r\n'
+# Usage: ./script_name.sh <URL>
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r\n'
