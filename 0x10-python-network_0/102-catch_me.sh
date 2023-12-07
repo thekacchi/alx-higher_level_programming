c762439558f989eaadeb56bc44d68191c9660e0f
Onyedikachi <thekacchi1@gmail.com>
2023-12-07 07:01:10 +0000

    il- gwuri

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Write a script yo dolicit "You hot me!"
-#curl -s -X POST http://0.0.0.0:5000/catch_me -d "You got me!" >/dev/null
-curl -sLX PUT "0.0.0.0:5000/catch_me" -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -sLX PUT "0.0.0.0:5000/catch_me" -d "user_id=98" -H "Origin: HolbertonSchool
