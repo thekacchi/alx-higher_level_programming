1cf5d68d2a38826885f9e1005b4e3d323a6ce1bd
Onyedikachi Eze <thekacchi1@gmail.com>
2024-01-06 16:00:51 -0800

    to me

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const axios = require('axios');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./2-statuscode.js <URL>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+axios.get(url)
+  .then(response => {
+    console.log('code:', response.status);
+  })
+  .catch(error => {
+    console.error(error.message);
+  });
